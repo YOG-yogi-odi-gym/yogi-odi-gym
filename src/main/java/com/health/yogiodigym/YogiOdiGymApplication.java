ddd501df0721669ee83a65c45599c9897f5ea07c
leedahoon <ekgns0508@gmail.com>
2025-02-10 19:12:34 +0900

    Chore: 프로젝트 세팅

---
+++ src/main/java/com/health/yogiodigym/YogiOdiGymApplication.java
@@ -0,0 +1,13 @@
+package com.health.yogiodigym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YogiOdiGymApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YogiOdiGymApplication.class, args);
+    }
+
+}
