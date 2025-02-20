395fbd89af6486942c13383690feb5e2018bd0fe
kimdohyun2 <38269527+kimdohyun2@users.noreply.github.com>
2025-02-20 10:44:21 +0900

    feat: OAuthUser커스텀
     - MemberOAuth2User Entity 추가(OAuth2에 Member정보를 포함하는 엔티티)
     - KakaoOAuth2UserService 변경(OAuth2인증 정보로 DB Member 정보 가져옴)

--- src/main/java/com/health/yogiodigym/member/service/KakaoOAuth2UserService.java
+++ src/main/java/com/health/yogiodigym/member/service/KakaoOAuth2UserService.java
@@ -1,35 +1,49 @@
 package com.health.yogiodigym.member.service;
 
-import java.util.Collections;
-import java.util.Map;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import com.health.yogiodigym.member.auth.Role;
+import com.health.yogiodigym.member.entity.Member;
+import com.health.yogiodigym.member.entity.MemberOAuth2User;
+import com.health.yogiodigym.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
-import jakarta.servlet.http.HttpSession;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+
+import java.util.EnumSet;
+import java.util.Map;
+import java.util.Set;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
 public class KakaoOAuth2UserService extends DefaultOAuth2UserService {
-
-    private final HttpSession httpSession;
-
+    private final MemberRepository memberRepository;
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
         Map<String, Object> attributes = oAuth2User.getAttributes();
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        String email = (String)kakaoAccount.get("email");
 
-        log.info("attributes :: " + attributes);
+        Member kakaoLoginMember = memberRepository.findByEmail(email);
+        if(kakaoLoginMember != null){
+            return new MemberOAuth2User(kakaoLoginMember, kakaoAccount);
+        }else{
+            Map<String, Object> profile = (Map<String, Object>) kakaoAccount.get("profile");
+            Set<Role> newMemberRoles = EnumSet.of(Role.ROLE_USER);
 
-        httpSession.setAttribute("login_info", attributes);
+            Member newKakaoMember = Member.builder()
+                    .name((String)profile.get("nickname"))
+                    .email((String)kakaoAccount.get("email"))
+                    .profile((String)profile.get("profile_image_url"))
+                    .roles(newMemberRoles)
+                    .status("ACTIVE")
+                    .build();
 
-        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority("ROLE_USER")),
-                oAuth2User.getAttributes(), "id");
+            return new MemberOAuth2User(memberRepository.save(newKakaoMember), kakaoAccount);
+        }
     }
 }
