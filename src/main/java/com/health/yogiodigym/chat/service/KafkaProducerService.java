b84d8f5bfa3ce6ee21480c8c0eef143701b66cc1
leedahoon <ekgns0508@gmail.com>
2025-02-20 11:48:39 +0900

    feat: Kafka producer/consumer 서비스 클래스 생성
    - groupId는 랜덤 문자열로 지정해 각 서버마다 다르게 값을 갖도록 설정

---
+++ src/main/java/com/health/yogiodigym/chat/service/KafkaProducerService.java
@@ -0,0 +1,18 @@
+package com.health.yogiodigym.chat.service;
+
+import com.health.yogiodigym.chat.dto.MessageDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaProducerService {
+
+    private final KafkaTemplate<String, MessageDto> kafkaTemplate;
+
+    public void sendMessage(String topic, MessageDto message) {
+        kafkaTemplate.send(topic, message);
+    }
+}
+
