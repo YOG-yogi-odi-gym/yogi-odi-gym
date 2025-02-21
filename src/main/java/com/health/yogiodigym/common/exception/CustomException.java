4dac277e84391af35a7b03ee4f14e8f37311bbd7
leedahoon <ekgns0508@gmail.com>
2025-02-21 15:24:53 +0900

    feat: 예외 관련 클래스 생성
    - 예외 클래스의 틀을 위한 추상 클래스 생성
    - 발생한 예외를 처리해주는 ExceptionHandler 생성(Rest Api에 대해서만 동작)

--- src/main/java/com/health/yogiodigym/common/exception/CustomException.java
+++ src/main/java/com/health/yogiodigym/common/exception/CustomException.java
@@ -3,12 +3,6 @@ package com.health.yogiodigym.common.exception;
 import org.springframework.http.HttpStatus;
 
 public abstract class CustomException extends RuntimeException {
-
-    private HttpStatus httpStatus;
-    private String message;
-
-    public CustomException(HttpStatus httpStatus, String message) {
-        super(message);
-        this.httpStatus = httpStatus;
-    }
+    abstract public HttpStatus getStatus();
+    abstract public String getMessage();
 }
