f49e05a6e403518c820e8c85a70f4d0cdf1b0950
Bhin <bbhinhb@gmail.com>
2025-02-24 20:44:09 +0900

    refactor: 불필요한 파일 삭제
    - HttpResponseDto.java 삭제
    - AdminController.java 코드 수정

--- src/main/java/com/health/yogiodigym/admin/controller/AdminController.java
+++ src/main/java/com/health/yogiodigym/admin/controller/AdminController.java
@@ -1,8 +1,10 @@
 package com.health.yogiodigym.admin.controller;
 
+import static com.health.yogiodigym.common.message.SuccessMessage.*;
+
 import com.health.yogiodigym.admin.dto.MemberDto.*;
 import com.health.yogiodigym.admin.service.AdminService;
-import com.health.yogiodigym.common.response.HttpResponseDto;
+import com.health.yogiodigym.common.response.HttpResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@Controller
+@RestController
 @RequestMapping("/api/admin")
 @RequiredArgsConstructor
 public class AdminController {
@@ -20,16 +22,15 @@ public class AdminController {
     private final AdminService adminService;
 
     @GetMapping("/member/search")
-    public ResponseEntity<?> findMembers(@RequestParam String keyword, Model model) {
+    public ResponseEntity<?> findMembers(@RequestParam String keyword) {
         List<MemberResponseDto> members = adminService.searchMembers(keyword);
-        model.addAttribute("members", members);
 
-        return ResponseEntity.ok().body(new HttpResponseDto(HttpStatus.OK.value(), members));
+        return ResponseEntity.ok().body(new HttpResponse(HttpStatus.OK, MEMBER_SEARCH_SUCCESS.getMessage(), members));
     }
 
     @PostMapping("/member/inactive")
     public ResponseEntity<?> setInactiveStatus(@RequestBody List<Long> memberIds) {
         adminService.setInactiveStatus(memberIds);
-        return ResponseEntity.ok().body(new HttpResponseDto(HttpStatus.OK.value(), "선택한 회원이 비활성화되었습니다."));
+        return ResponseEntity.ok().body(new HttpResponse(HttpStatus.OK, MEMBER_STATUS_CHANGE_SUCCESS.getMessage(), null));
     }
 }
