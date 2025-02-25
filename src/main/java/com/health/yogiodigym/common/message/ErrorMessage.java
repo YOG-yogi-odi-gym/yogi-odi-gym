a9d3793fef51f4dcaf79fd81e31ef19344348b66
leedahoon <ekgns0508@gmail.com>
2025-02-25 10:41:09 +0900

    feat: 채팅방 관련 실패 메시지 추가

--- src/main/java/com/health/yogiodigym/common/message/ErrorMessage.java
+++ src/main/java/com/health/yogiodigym/common/message/ErrorMessage.java
@@ -6,7 +6,12 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ErrorMessage {
-    SERVER_ERROR("서버 내부 오류");
+    SERVER_ERROR("서버 내부 오류"),
+    MEMBER_NOT_FOUND("회원이 존재하지 않습니다."),
+    CHAT_ROOM_NOT_FOUND("채팅방이 존재하지 않습니다."),
+    LESSON_NOT_FOUND("강의가 존재하지 않습니다."),
+    MEMBER_NOT_IN_CHAT_ROOM("채팅방 참여자가 아닙니다."),
+    ALREADY_CHAT_PARTICIPANT("이미 채팅방에 참여중입니다.");
 
     private final String message;
 }
