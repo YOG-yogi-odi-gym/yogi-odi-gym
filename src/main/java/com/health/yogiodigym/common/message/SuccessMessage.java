e369de76de615bf0ecfe656415bcc583ba343b0f
leedahoon <ekgns0508@gmail.com>
2025-02-25 10:41:01 +0900

    feat: 채팅방 관련 성공 메시지 추가

--- src/main/java/com/health/yogiodigym/common/message/SuccessMessage.java
+++ src/main/java/com/health/yogiodigym/common/message/SuccessMessage.java
@@ -6,7 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum SuccessMessage {
-    JOIN_SUCCESS("회원가입에 성공하였습니다.");
+    JOIN_SUCCESS("회원가입에 성공하였습니다."),
+    CREATE_CHAT_ROOMS_SUCCESS("채팅방 생성에 성공하였습니다."),
+    GET_MY_CHAT_ROOMS_SUCCESS("채팅방 목록 조회에 성공하였습니다."),
+    KICK_MEMBER_SUCCESS("회원 강퇴에 성공하였습니다."),
+    QUIT_CHAT_ROOM_SUCCESS("채팅방 나가기에 성공하였습니다.");
 
     private final String message;
 }
