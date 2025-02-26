d158329abe7ad66525fece327b8eee0ef68ef644
leedahoon <ekgns0508@gmail.com>
2025-02-26 21:20:34 +0900

    fix: SuccessMessage 추가

--- src/main/java/com/health/yogiodigym/common/message/SuccessMessage.java
+++ src/main/java/com/health/yogiodigym/common/message/SuccessMessage.java
@@ -9,7 +9,7 @@ public enum SuccessMessage {
     CREATE_CHAT_ROOMS_SUCCESS("채팅방 생성에 성공하였습니다."),
     GET_MY_CHAT_ROOMS_SUCCESS("채팅방 목록 조회에 성공하였습니다."),
     KICK_MEMBER_SUCCESS("회원 강퇴에 성공하였습니다."),
-    QUIT_CHAT_ROOM_SUCCESS("채팅방 나가기에 성공하였습니다.");
+    QUIT_CHAT_ROOM_SUCCESS("채팅방 나가기에 성공하였습니다."),
     REGIST_SUCCESS("회원가입에 성공하였습니다."),
     MEMBER_SEARCH_SUCCESS("회원목록 검색에 성공하였습니다."),
     MEMBER_STATUS_CHANGE_SUCCESS("회원 비활성화 성공하였습니다.");
