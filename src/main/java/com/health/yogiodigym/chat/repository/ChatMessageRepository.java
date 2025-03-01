c09007e2dfed59d7817adb2624a132e011919025
leedahoon <ekgns0508@gmail.com>
2025-03-01 20:45:40 +0900

    refactor: 채팅메시지 repository 리팩토링
    - JPQL -> JPA 쿼리메서드로 수정

--- src/main/java/com/health/yogiodigym/chat/repository/ChatMessageRepository.java
+++ src/main/java/com/health/yogiodigym/chat/repository/ChatMessageRepository.java
@@ -2,6 +2,10 @@ package com.health.yogiodigym.chat.repository;
 
 import com.health.yogiodigym.chat.entity.ChatMessage;
 import com.health.yogiodigym.chat.entity.ChatRoom;
+import com.health.yogiodigym.member.entity.Member;
+import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,4 +16,14 @@ public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> 
     @Modifying
     @Query("DELETE FROM ChatMessage cm WHERE cm.chatRoom = :chatRoom")
     void deleteAllByChatRoomInBatch(@Param("chatRoom") ChatRoom chatRoom);
+
+    List<ChatMessage> findByMemberAndChatRoomAndIdGreaterThan(Member member, ChatRoom chatRoom, Long lastReadMessageId);
+
+    Page<ChatMessage> findByMemberAndChatRoomAndIdLessThanEqualOrderByIdDesc(
+                            Member member,
+                            ChatRoom chatRoom,
+                            Long lastReadMessageId,
+                            Pageable pageable);
+
+    Long countByMemberAndChatRoom(Member member, ChatRoom chatRoom);
 }
