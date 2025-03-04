06e3d337fb074ce036cd405311e8e56d6e1b4b2b
kimdohyun2 <38269527+kimdohyun2@users.noreply.github.com>
2025-03-04 10:29:41 +0900

    merge: feat#77, pr#69 v2

--- src/main/java/com/health/yogiodigym/my/controller/rest/Mycontroller.java
+++ src/main/java/com/health/yogiodigym/my/controller/rest/Mycontroller.java
@@ -1,6 +1,10 @@
 package com.health.yogiodigym.my.controller.rest;
 
+import com.health.yogiodigym.board.dto.BoardDto.*;
+import com.health.yogiodigym.board.service.BoardService;
 import com.health.yogiodigym.common.response.HttpResponse;
+import com.health.yogiodigym.lesson.dto.LessonDto.*;
+import com.health.yogiodigym.lesson.service.LessonService;
 import com.health.yogiodigym.member.entity.MemberOAuth2User;
 import com.health.yogiodigym.member.service.MemberService;
 import com.health.yogiodigym.member.service.NCPStorageService;
@@ -12,22 +16,31 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import static com.health.yogiodigym.common.message.SuccessMessage.*;
 
 @Slf4j
 @RestController
-@RequiredArgsConstructor
 @RequestMapping("/api/my")
-public class Mycontroller {
+@RequiredArgsConstructor
+public class MyController {
 
+    private final BoardService boardService;
+    private final LessonService lessonService;
     private final MemberService memberService;
     private final NCPStorageService ncpStorageService;
 
@@ -50,7 +63,7 @@ public class Mycontroller {
 
     @PutMapping("/info")
     public ResponseEntity<?> info(@Valid @RequestBody UpdateMemberDto updateMemberDto, @AuthenticationPrincipal MemberOAuth2User principal,
-                                        HttpServletRequest request, HttpServletResponse response) {
+                                  HttpServletRequest request, HttpServletResponse response) {
         log.info("update member: {}", updateMemberDto);
 
         memberService.updateMember(updateMemberDto);
@@ -91,5 +104,37 @@ public class Mycontroller {
         memberService.enrollMaster(certificates);
 
         return ResponseEntity.ok().body(new HttpResponse(HttpStatus.OK, ENROLL_MASTER_SUCCESS.getMessage(), null));
+    }
+
+    @GetMapping("/board")
+    public ResponseEntity<?> searchMyBoard(@RequestParam(required = false) String boardKeyword,
+                                           @RequestParam(required = false) String searchColumn,
+                                           @RequestParam(required = false) List<Long> categories,
+                                           @RequestParam(defaultValue = "0") int page,
+                                           @RequestParam(defaultValue = "10") int size,
+                                           @AuthenticationPrincipal MemberOAuth2User loginUser) {
+        System.out.println(loginUser.getMember().getId());
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC, "id"));
+        Page<BoardDetailDto> boards = boardService.searchMyBoards(loginUser.getMember().getId(), boardKeyword, searchColumn, categories, pageable);
+
+        return ResponseEntity.ok().body(new HttpResponse(HttpStatus.OK, SEARCH_BOARD_SUCCESS.getMessage(), boards));
+    }
+
+    @GetMapping("/lesson")
+    public ResponseEntity<?> searchLessons(@RequestParam(required = false) String lessonKeyword,
+                                           @RequestParam(required = false) String searchColumn,
+                                           @RequestParam(required = false) Integer days,
+                                           @RequestParam(required = false) List<Long> categories,
+                                           @RequestParam(defaultValue = "0") int page,
+                                           @RequestParam(defaultValue = "10") int size,
+                                           @AuthenticationPrincipal MemberOAuth2User loginUser) {
+
+        System.out.println(loginUser.getMember().getId());
+
+        Pageable pageable = PageRequest.of(page, size, Sort.by(Sort.Direction.DESC,"id"));
+        Page<LessonSearchDto> lessons = lessonService.searchMyLessons(loginUser.getMember().getId(), lessonKeyword, searchColumn, days, categories, pageable);
+
+        return ResponseEntity.ok().body(new HttpResponse(HttpStatus.OK, SEARCH_LESSON_SUCCESS.getMessage(), lessons));
     }
 }
