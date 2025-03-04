df27fea0e59e81f994ad8cac93fb7f9d7ae71ecc
kimdohyun2 <38269527+kimdohyun2@users.noreply.github.com>
2025-03-04 16:05:26 +0900

    fix: 중복클래스 제거

--- src/main/java/com/health/yogiodigym/my/repository/MemberToMasterRepository.java
+++ src/main/java/com/health/yogiodigym/my/repository/MemberToMasterRepository.java
@@ -1,8 +1,11 @@
 package com.health.yogiodigym.my.repository;
 
+import com.health.yogiodigym.member.entity.Member;
 import com.health.yogiodigym.my.entity.MemberToMaster;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberToMasterRepository extends JpaRepository<MemberToMaster, Long> {
+import java.util.Optional;
 
+public interface MemberToMasterRepository extends JpaRepository<MemberToMaster, Long> {
+    Optional<MemberToMaster> findByMemberId(Long memberId);
 }
