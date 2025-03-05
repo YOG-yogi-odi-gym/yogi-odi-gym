709050e9ee777d6f69b69f17aab36dc3ec5de50d
leedahoon <ekgns0508@gmail.com>
2025-03-05 15:06:37 +0900

    feat: 대시보드 컨트롤러 model 데이터 추가

--- src/main/java/com/health/yogiodigym/member/controller/view/MainViewController.java
+++ src/main/java/com/health/yogiodigym/member/controller/view/MainViewController.java
@@ -1,14 +1,22 @@
 package com.health.yogiodigym.member.controller.view;
 
+import com.health.yogiodigym.member.entity.MemberOAuth2User;
+import com.health.yogiodigym.member.service.GraphAverageService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor
 public class MainViewController {
+
+    private final GraphAverageService graphAverageService;
 
     @GetMapping("/")
     public String main(Authentication authentication) {
@@ -20,7 +28,13 @@ public class MainViewController {
     }
 
     @GetMapping("/dashboard")
-    public String dashoboard() {
+    public String dashboard(@AuthenticationPrincipal MemberOAuth2User loginUser, Model model) {
+
+        model.addAttribute("calorieAverages", graphAverageService.getCalorieAverage());
+        model.addAttribute("exerciseTimeAverages", graphAverageService.getExerciseTimeAverage());
+        model.addAttribute("myCalories", graphAverageService.getMyPreviousDateCalorie(loginUser.getMember()));
+        model.addAttribute("myExerciseTimeAverages", graphAverageService.getMyPreviousDateExerciseTime(loginUser.getMember()));
+
         return "dashboard";
     }
 
